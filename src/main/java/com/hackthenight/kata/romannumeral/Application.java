a1637ac32949adbc2b867192bbadfe3527033cbc
Alec Doran-Twyford <alec@alectronic.co>
2019-05-05 20:19:06 +0100

    Initial commit

---
+++ src/main/java/com/hackthenight/kata/romannumeral/Application.java
@@ -0,0 +1,9 @@
+package com.hackthenight.kata.romannumeral;
+
+public class Application {
+
+    public void main(String... args) {
+        System.out.println("Hello world");
+    }
+
+}
---
+++ src/test/java/com/hackthenight/kata/romannumeral/ApplicationTest.java
@@ -0,0 +1,7 @@
+package com.hackthenight.kata.romannumeral;
+
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+}
