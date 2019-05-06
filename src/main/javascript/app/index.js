0769ad56f367506d4e75488e8512369680a8cae4
Alec Doran-Twyford <alec@alectronic.co>
2019-05-06 14:43:45 +0100

    apply initial code (still failing)

--- src/main/javascript/app/index.js
+++ src/main/javascript/app/index.js
@@ -1,2 +1,61 @@
-const sum = (a, b) => { return a + b; }
-module.exports = sum;
+const numeralMap = {
+    4: "IV",
+    9: "IX",
+    40: "XL",
+    90: "XC"
+};
+
+const numeralList = [
+    {arabic: 1000, roman:"M"},
+    {arabic: 500, roman:"D"},
+    {arabic: 100, roman:"C"},
+    {arabic: 50, roman:"L"},
+    {arabic: 10, roman:"X"},
+    {arabic: 5, roman:"V"},
+    {arabic: 1, roman:"I"}
+];
+
+
+module.exports = (args) => {
+    if (isNaN(args) || args === null) {
+        throw "Not a number";
+    }
+
+    return convertToRoman(args, 0);
+};
+
+function convertToRoman(decimalNumber, mappedNumber){
+    const conversion = numeralMap[decimalNumber];
+
+    // if (conversion !== undefined) return conversion;
+
+    // if there's a roman numeral for the number we're on, use it
+    // if there's a difference between the roman numeral we're on
+    // that is equal to another roman numeral, place it on the left
+    // X = 10
+    // 9 = one less than tem
+    // we have a roman numeral for one and a roman numeral for 10
+    // 9 = IX
+    // 9 = 10 - 1
+    // 1 = I in roman numerals
+
+    let romanString = "";
+
+    if (mappedNumber < numeralList.length) {
+        const whole = decimalNumber / numeralList[mappedNumber].arabic;
+        const remainder = decimalNumber % numeralList[mappedNumber].arabic;
+
+        romanString = romanString + repeatRomanString(whole,numeralList[mappedNumber].roman);
+
+        if (0 < remainder) {
+            const newMappedNumber = mappedNumber+1;
+            romanString = romanString + convertToRoman(remainder, newMappedNumber);
+        }
+    }
+
+    return romanString;
+}
+
+const repeatRomanString = (numberOfTimes, romanNumeral) => {
+    return ((numberOfTimes > 0) ? romanNumeral.repeat(numberOfTimes) : "");
+}
--- src/main/javascript/app/spec/app.spec.js
+++ src/main/javascript/app/spec/app.spec.js
@@ -18,7 +18,56 @@ describe("convertNumberToRomanNumerals", () => {
                 ${8}  | ${"VIII"}
                 ${9}  | ${"IX"}
                 ${10} | ${"X"}
-              `('should return $result when input $arg', ({arg, result}) => {
+                ${11} | ${"XI"}
+                ${12} | ${"XII"}
+                ${13} | ${"XIII"}
+                ${14} | ${"XIV"}
+                ${15} | ${"XV"}
+                ${16} | ${"XVI"}
+                ${17} | ${"XVII"}
+                ${18} | ${"XVIII"}
+                ${19} | ${"XIX"}
+                ${20} | ${"XX"}
+                ${21} | ${"XXI"}
+                ${22} | ${"XXII"}
+                ${23} | ${"XXIII"}
+                ${24} | ${"XXIV"}
+                ${25} | ${"XXV"}
+                ${26} | ${"XXVI"}
+                ${27} | ${"XXVII"}
+                ${28} | ${"XXVIII"}
+                ${29} | ${"XXIX"}
+                ${30} | ${"XXX"}
+                ${31} | ${"XXXI"}
+                ${32} | ${"XXXII"}
+                ${33} | ${"XXXIII"}
+                ${34} | ${"XXXIV"}
+                ${35} | ${"XXXV"}
+                ${36} | ${"XXXVI"}
+                ${37} | ${"XXXVII"}
+                ${38} | ${"XXXVIII"}
+                ${39} | ${"XXXIX"}
+                ${40} | ${"XL"}
+                ${41} | ${"XLI"}
+                ${42} | ${"XLII"}
+                ${43} | ${"XLIII"}
+                ${44} | ${"XLIV"}
+                ${45} | ${"XLV"}
+                ${46} | ${"XLVI"}
+                ${47} | ${"XLVII"}
+                ${48} | ${"XLVIII"}
+                ${49} | ${"XLIX"}
+                ${50} | ${"L"}
+                ${51} | ${"LI"}
+                ${52} | ${"LII"}
+                ${53} | ${"LIII"}
+                ${54} | ${"LIV"}
+                ${55} | ${"LV"}
+                ${56} | ${"LVI"}
+                ${59} | ${"LIX"}
+                ${60} | ${"LX"}
+                ${100} | ${"C"}
+                `('should return $result when input $arg', ({arg, result}) => {
                 expect(convertNumberToRomanNumeral(arg)).toBe(result);
             });
         });
