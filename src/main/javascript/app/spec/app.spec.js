979312a7907e810d4f155d17b50c79427c4c54e6
Alec Doran-Twyford <alec@alectronic.co>
2019-05-06 14:42:29 +0100

    write up to 10 and invalid params

--- src/main/javascript/app/spec/app.spec.js
+++ src/main/javascript/app/spec/app.spec.js
@@ -1,5 +1,42 @@
-const sum = require('../index');
+const convertNumberToRomanNumeral = require('../index');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+describe("convertNumberToRomanNumerals", () => {
+
+    describe("when i input", () => {
+
+        describe("a valid parameter", () => {
+
+            it.each`
+                arg   | result
+                ${1}  | ${"I"}
+                ${2}  | ${"II"}
+                ${3}  | ${"III"}
+                ${4}  | ${"IV"}
+                ${5}  | ${"V"}
+                ${6}  | ${"VI"}
+                ${7}  | ${"VII"}
+                ${8}  | ${"VIII"}
+                ${9}  | ${"IX"}
+                ${10} | ${"X"}
+              `('should return $result when input $arg', ({arg, result}) => {
+                expect(convertNumberToRomanNumeral(arg)).toBe(result);
+            });
+        });
+
+        describe("an invalid parameter", () => {
+
+            it("should throw an error when input is string", () => {
+                expect(() => {
+                    convertNumberToRomanNumeral("test")
+                }).toThrow("Not a number");
+            });
+
+            it("should throw an error when input is null", () => {
+                expect(() => {
+                    convertNumberToRomanNumeral(null)
+                }).toThrow("Not a number");
+            });
+
+        });
+    });
 });
